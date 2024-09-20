1fa888e9372cb4c6d5c66a1b32745b9ea3f00a5c
Allan Wang <allan.wang@fevolution.com>
2024-09-21 01:45:36 +0800

    Add POST orders API & GET orders/id API with OrderRequest.

--- app/Http/Controllers/api/OrderController.php
+++ app/Http/Controllers/api/OrderController.php
@@ -1,13 +1,13 @@
 <?php
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\api;
 
 use App\Events\OrderCreated;
-use App\Http\Requests\StoreOrderRequest;
+use App\Http\Requests\OrderRequest;
 use Illuminate\Http\Response;
 
 class OrderController extends Controller
 {
-    public function store(StoreOrderRequest $request)
+    public function store(OrderRequest $request)
     {
         event(new OrderCreated($request->validated()));
         return response()->json(['message' => 'Order received'], Response::HTTP_OK);
@@ -17,5 +17,6 @@ class OrderController extends Controller
     {
         // 根據不同的貨幣查詢對應的資料表
         // 此處省略具體實作
+        echo $id;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
-use App\Http\Controllers\OrderController;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\api\OrderController;
 
 Route::post('/orders', [OrderController::class, 'store']);
 Route::get('/orders/{id}', [OrderController::class, 'show']);
